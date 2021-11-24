d8462f8e766acbedf4a158db0295da0cdb5e8c6b
super-jordan <j0rd4nshippuden@gmail.com>
2021-11-24 12:28:49 -0600

    first commituu

---
+++ Secundaria_index1.php
@@ -0,0 +1,272 @@
+<?php
+	$paginaweb = 'primer_nivel';
+    date_default_timezone_set('America/Mexico_City');
+    include '../extensiones/funcion_errores.php';
+?>
+
+
+<!DOCTYPE html>
+  <html lang="es">
+    <head>
+
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+      <meta http-equiv="X-UA-Compatible" content="ie-edge">
+	  <meta name="robots" content="index, follow">
+	  <meta name="description" content="Estudia Licenciatura Derecho en Tapachula, Tuxtla Gutiérrez. Yajalón, Tapilula, Reforma, Campeche en el Sistema Educativo Universitario Azteca" />
+	  <meta name="keywords" content="mercantil,laboral,juridica"/>
+	  <meta name="author" content="Dirección de medios virtuales y sistemas" />
+	  <meta name="copyright" content="Universidad SEUAT" />
+	  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+	  <!--<link  href="/css/estilos_J.css" rel="stylesheet">-->
+    <link rel="stylesheet" href="../css/estilos.css">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.6.1/font/bootstrap-icons.css">
+    <style>
+      li{
+        font-size: 19px;
+        margin: 5px;
+      }
+      p{
+        font-size: 20px;
+        margin: 5px;
+      }
+
+    </style>
+    
+
+    <title>Secundaria</title>
+  </head>
+  <body style="background: #E5E7E9;">
+
+    <div class=" container-fluid">
+      <div class="cabeza row">
+        <div class="col-sm-12 col-md-1 "><p class="ops" >CEJHET</p></div>
+        <div class="col-sm-12 col-md-1">|</div>
+        <div class="col-sm-12 col-md-2 "><p class="ops"> 01 800 8908403</p></div>
+        <div class="col-sm-12 col-md-1">|</div>
+        <div class="col-sm-12 col-md-2"><p class="ops"> inscripcion@seuat.mx</p></div>
+        <div class="col-sm-12 col-md-1">|</div>
+        <div class="col-sm-12 col-md-3"><p class="ops">2da norte entre 4ta y 5ta oriente</p></div>
+    
+      </div>
+    </div>
+
+    <hr>
+
+
+    <div class="container-fluid">
+        <!--Banner de secundaria-->
+        <div class="row">
+            <div class="col-sm-12 col-md-12">
+
+                <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
+                    <div class="carousel-inner">
+
+                      <div class="carousel-item active">
+                          <img src="../images/secundaria-op.png" class="d-block w-100" alt="..." width="100%" height="500px">
+                      </div>
+
+                      <div class="carousel-item">
+                        <img src="../images/bachillerato_op.jpg" class="d-block w-100" alt="..." width="100%" height="500px">
+                      </div>
+
+                      <div class="carousel-item">
+                        <img src="../images/admisiones.png" class="d-block w-100" alt="..." width="100%" height="500px">
+                      </div>
+                    </div>
+                </div>
+
+            </div>
+         </div>
+       </div>
+
+
+        <p><br /></p>
+        <!--Cuadros-->
+        <div class="row">
+            <!--Formulario-->
+            
+             <div class="separar col-sm-12 col-md-3">
+                 <div class="form">
+                    <h2 style="text-align: center; margin-top: 10%;">Rellenar los campos</h2>
+        <br/>
+                    <div class=" formulario2">
+                    <label for="Nombre" class="form-label">Nombre Completo</label>
+                    <input type="text" class="form-control" id="Nombre" placeholder="Nombre y Apellido">
+                    </div>
+
+                    <div class="form-row formulario2">
+                    <label for="E-mail" class="form-label">E-mail</label>
+                    <input type="e-mail" class="form-control" id="E-mail" placeholder="micorreo@ejemplo.com">
+                    </div>
+
+                    <div class="form-row formulario2">
+                    <label for="Telèfono" class="form-label">Telèfono</label>
+                    <input type="text" class="form-control" id="Telèfono" placeholder="Telèfono 10 digitos">
+                    </div>
+
+                    <div class="form-row formulario2">
+                    <label for="Asunto" class="form-label">Asunto</label>
+                    <input type="text" class="form-control" id="Asunto" placeholder="Asunto Principal">
+                    </div>
+  
+                    <div class="form-row  formulario2">
+                    <label for="Plantel" class="form-label">Plantel</label>
+                    <input type="text" class="form-control" id="Plantel" placeholder="--Elige un Plantel--">
+                    </div>
+ 
+
+                    <div class="form-row  formulario2">
+                    <label for="Mensaje" class="form-label">Mensaje</label>
+                    <textarea class="form-control" id="Mensaje" rows="3"></textarea>
+                    </div>
+
+                    <div>
+                    <button style="margin-top: 10%; margin-left: 6%;" type="button" class="botonfor">Enviar información</button>	  
+                    </div>
+                </div>
+            </div>
+
+        
+            
+
+
+            <!--Cuadros-->
+          <div class="separa col-sm-12 col-md-6">
+        
+            <!--Benefisios-->
+            
+           
+            <p><br /></p>
+            
+
+            <!--Requisitos-->
+
+            <div style=" margin-left: 10%;" class="ppp cajas row">
+              <div class="col-sm-12 col-md-5" style="right: 25px;">
+               <img src="../images/11.jpg" alt="" width="100%" height="100%" >
+              </div>
+              <div class="col-sm-12 col-md-7">
+               <h3>Beneficios de estudiar con nosotros:</h3>
+               <br/>
+               <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati eveniet quisquam illo ut inventore cupiditate odio consequuntur aliquam? Laudantium, distinctio consequatur? Accusantium, rem numquam ducimus dolor consequatur quisquam facilis quas!
+              </p>
+              </div>
+  
+            </div>
+
+            <p><br /><br /></p>
+
+            <div class="separa col-sm-12 col-md-10">
+              <div class="tab_secu cajas row">
+                
+                <nav class="nav nav-pills">
+                  <div class="nav nav-item" id="nav-tab" role="tablist">
+                    <a class="nav-link active" id="nav-home-tab" data-toggle="tab" href="#Diplomados" role="tab" aria-controls="nav-home" aria-selected="true">Diplomados</a>
+                    <a class="nav-link" id="nav-profile-tab" data-toggle="tab" href="#Requisitos" role="tab" aria-controls="nav-profile" aria-selected="false">Requisitos</a>
+                    
+                  </div>
+                </nav>
+                
+                <div class="tab-content" id="nav-tabContent">
+                  <p><br /></p>
+                  <div class="tab-pane fade show active" id="Diplomados" role="tabpanel" aria-labelledby="nav-home-tab">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illo quos porro voluptate nesciunt saepe, perspiciatis animi amet id mollitia corporis recusandae laudantium quod at unde officia. Sequi voluptates doloribus ex.
+                  Eum quo obcaecati beatae quisquam veritatis! Adipisci quia laborum commodi sed odit non expedita, eaque voluptatum assumenda officia, sapiente repudiandae repellendus maxime facilis minus? Inventore ipsam accusamus asperiores officia aliquid.</div>
+                  <div class="tab-pane fade" id="Requisitos" role="tabpanel" aria-labelledby="nav-profile-tab">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestias laborum similique quibusdam, velit incidunt, eveniet odit repudiandae vero consequuntur unde illum fugit rem voluptate et! Ex, molestiae iure. Accusantium, omnis.
+                  Iure eligendi quod odit consequuntur quidem illo placeat repellat culpa debitis voluptatum hic soluta distinctio fuga recusandae labore sit beatae, vel doloremque nesciunt in quasi ut voluptatibus. Commodi, veritatis quos?</div>
+                 
+                  </div>
+              </div>
+          </div>
+
+         
+          <p><br /><br /></p>
+    
+        <div class="row">
+         <div class="Diplomados col-sm-12 col-md-11">
+          <h3>Plan de estudios:</h3>
+          <br/>
+          <p>Et ullamco esse veniam incididunt non incididunt. Est ullamco ipsum velit velit pariatur culpa. Ullamco aliqua non aliqua ea aute officia magna veniam minim laborum dolor esse nisi. Cupidatat laboris ut nisi amet eiusmod in velit ea aliqua commodo. Laborum occaecat ut ex elit ut cillum officia irure aliquip ut minim dolor.</p>
+         </div>
+       </div>
+
+
+
+          <p><br /></p>
+
+          
+         </div>
+
+
+
+</div>
+       
+
+    
+
+    
+
+    <!--Seccion de noticias-->
+
+    <p><br /><br /><br /></p>
+    <div class="container-fluid">
+      <h3 style="text-align: center; color: #3498DB; font-size: 40px;">Noticias:</h3>
+        <div class="row">
+          <div class="col-sm-12 col-md-2"></div>
+          
+
+          <div class="card marCard" style="width: 18rem;">
+            <img src="../images/11.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Noticia 1</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <a href="#" class="btn btn-primary">Ver mas</a>
+            </div>
+          </div>
+
+    
+          <div class="card marCard" style="width: 18rem;">
+            <img src="../images/licenciatura_opt.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Noticia 2</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <a href="#" class="btn btn-primary">Ver mas</a>
+            </div>
+          </div>
+       
+
+          <div class="card marCard" style="width: 18rem;">
+            <img src="../images/11.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Noticia 3</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <a href="#" class="btn btn-primary">Ver mas</a>
+            </div>
+          </div>
+
+      </div>
+    </div>
+    
+    <footer>
+      
+    </footer>
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-fQybjgWLrvvRgtW6bFlB7jaZrFsaBXjsOMm/tB9LTS58ONXgqbR9W8oWht/amnpF" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.min.js" integrity="sha384-VHvPCCyXqtD5DqJeNxl2dtTyhF78xXNXdkwX1CZeRusQfRKp+tA7hAShOK/B/fQ2" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
+<?php include '../extensiones/scripts.php'; ?>
+<?php include '../extensiones/pie_de_pagina.php'; ?>
